3d394d34e6a113c49549891bcc321b5c70432f40
erdenerunl <erdenerunale@gmail.com>
2020-12-18 00:27:59 +0300

    elementary components were created.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Customer</h1>
+    <hr />
+    <div class="container">
+      <input
+        type="text"
+        class="form-group"
+        placeholder="First name..."
+        v-model="formData.firstName"
+      />
+      <input
+        type="text"
+        class="form-group"
+        placeholder="Last name..."
+        v-model="formData.lastName"
+      />
+      <input
+        type="text"
+        class="form-group"
+        placeholder="Email..."
+        v-model="formData.email"
+      />
+      <button class="btn btn-primary" :class="addDisabled" @click="addCustomer">Add Customer</button>
+    </div>
+    <customer-list :customers="customers" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomerList from './components/CustomerList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomerList
+  },
+  data() {
+    return {
+      formData: {
+        firstName: "",
+        lastName: "",
+        email: "",
+      },
+      customers: [],
+    };
+  },
+  methods: {
+    clearForm() {
+      this.formData = {
+        firstName: "",
+        lastName: "",
+        email: "",
+      };
+    },
+    addCustomer() {
+      this.customers.push({
+        id: this.customers.length + 1,
+        ...this.formData,
+      });
+      console.log(this.customers);
+      this.clearForm();
+    },
+  },
+  computed : {
+    addDisabled(){
+      return {
+        'disabled' : this.customers.firstName === '' || 
+        this.customers.lastName === '' || 
+        this.customers.email === ''
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1{
+  margin: 20px 60px;
+}
+input {
+  width: 100%;
+  margin-bottom: 5px;
+}
+button{
+  width: 100%;
 }
 </style>
---
+++ src/components/CustomerList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="container">
+      <input type="text" class="mt-4" v-model="searchText" placeholder="Search customer...">
+      <table class="table my-4">
+        <thead>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th class="text-center">View</th>
+        </thead>
+        <tbody>
+          <tr v-for="customer in customers" :key="customer.id">
+            <td>{{ customer.firstName }}</td>
+            <td>{{ customer.lastName }}</td>
+            <td>{{ customer.email }}</td>
+            <button class="btn btn-outline-primary"> View </button>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["customers"],
+
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
